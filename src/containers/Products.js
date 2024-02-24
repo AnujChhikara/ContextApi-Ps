a372e394fdcfd51931da2d879131d631b4e9a8cc
AnujChhikara <anujchhikara777@gmail.com>
2024-02-24 14:32:32 +0530

    first commit

---
+++ src/containers/Products.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+
+
+import ProductItem from '../components/Products/ProductItem';
+import {ProductContext} from '../context/products-context'
+import './Products.css';
+
+const Products = props => {
+const {products} =  useContext(ProductContext)
+ 
+  return (
+    <ul className="products-list">
+      {products.map(prod => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Products;
